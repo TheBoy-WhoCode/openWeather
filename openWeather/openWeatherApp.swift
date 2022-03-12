eaaa648f0ec99130cee48a9151664dcb827ec777
TheBoy-WhoCode <jiten.pateljp2001@gmail.com>
2022-03-12 19:41:00 +0530

    Initial Commit

---
+++ openWeather/openWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  openWeatherApp.swift
+//  openWeather
+//
+//  Created by Jiten Patel on 12/03/22.
+//
+
+import SwiftUI
+
+@main
+struct openWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
